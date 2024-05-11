13483ad5abec169ec912329f06fce5b3134376f9
ysihak-B <ysihakbazezew@gmail.com>
2024-05-11 14:29:57 +0300

    adding quiz controllers and routes

--- CPCA-back/controllers/index.js
+++ CPCA-back/controllers/index.js
@@ -1,4 +1,5 @@
 export * from "./userController.js";
 export * from "./courseControllers.js";
 export * from './lessonControllers.js'; 
-export * from './question.controller.js'; 
+export * from './question.controller.js';
+export * from './quizControllers.js' 
