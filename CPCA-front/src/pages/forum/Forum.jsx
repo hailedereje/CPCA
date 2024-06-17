5a5e7723ae2f694e99ed3423e297281f8ee9eb60
hailedereje <hailedereje88@gmail.com>
2024-06-17 10:25:03 +0300

    updating forum ui

--- CPCA-front/src/pages/forum/Forum.jsx
+++ CPCA-front/src/pages/forum/Forum.jsx
@@ -8,7 +8,6 @@ import { Toaster } from "react-hot-toast";
 import Loading from "../../components/Loading";
 import NothingHere from "../../components/NothingHere";
 import { useContext, useEffect, useState } from "react";
-import LikeDislikeComponent from "../../icons/LikeDislike";
 import { useSelector, useDispatch } from "react-redux";
 import { useQuery } from "react-query";
 import DiscussionContext from "@/context/DiscussionContext";
@@ -89,33 +88,20 @@ const Forum = () => {
   if (isLoading) return <Loading />;
 
   return (
-    <div
-      className="flex flex-col items-center w-1/2 bg-slate-100  h-screen overflow-y-auto"
-    >
+    <div className="flex flex-col items-center w-full h-screen overflow-y-auto bg-slate-100">
       <Toaster />
       {questions.length > 0 &&
         questions.map((question, index) => {
           return (
             <div
               key={index}
-              className="flex flex-col 
-              items-end p-3 md:p-4 border-b border-gray-200 w-full"
+              className="flex flex-col items-end p-3 md:p-4 border-b border-gray-200 w-full"
             >
-              <div
-                className="w-full bg-white 
-                p-4 md:p-5 rounded-lg shadow-md flex items-start gap-5"
-              >
-                <div className="left-section space-y-1 text-center">
-                  <LikeDislikeComponent question={question} />
-                </div>
+              <div className="w-full bg-white p-4 md:p-5 rounded-lg shadow-md flex items-start gap-5">
                 <div className="right-section w-full">
-                  <h1 className="text-base md:text-lg ">
-                    {question?.question}
-                  </h1>
-                  <p className="text-sm md:text-base">
-                    {question?.description}
-                  </p>
-                  <hr className="my-2"/>
+                  <h1 className="text-lg font-semibold text-gray-800">{question?.question}</h1>
+                  <p className="text-base text-gray-600">{question?.description}</p>
+                  <hr className="my-2" />
                   <UserInfo
                     openId={openId}
                     index={index + 1}
@@ -128,27 +114,24 @@ const Forum = () => {
                 <>
                   {question?.replies?.map((answer) => {
                     return (
-                      <div key={answer._id} className="flex items-center gap-4">
+                      <div key={answer._id} className="flex items-center gap-4 mt-2">
                         <img
                           className="h-4 md:h-6 w-4 md:w-6"
                           src="https://cdn.icon-icons.com/icons2/2596/PNG/512/nested_arrows_icon_155086.png"
                           alt=""
                         />
-                        <div className="bg-white   max-w-xl p-5 rounded-lg shadow-md flex flex-col items-start gap-5 mt-2">
-                          <p className="text-inherit">{answer?.reply}</p>
+                        <div className="bg-white max-w-xl p-5 rounded-lg shadow-md flex flex-col items-start gap-5">
+                          <p className="text-gray-800">{answer?.reply}</p>
                           <UserInfo answer={answer} />
                         </div>
                       </div>
                     );
                   })}
-                  <div
-                    className="w-full bg-white  flex items-center gap-4
-       px-5 py-2 rounded-lg shadow-md  mt-2"
-                  >
+                  <div className="w-full bg-white flex items-center gap-4 px-5 py-2 rounded-lg shadow-md mt-2">
                     <Write />
                     <input
                       onChange={(e) => setAnswer(e.target.value)}
-                      className="w-full h-10 border-none outline-none rounded-md py-1 px-2 "
+                      className="w-full h-10 border border-gray-300 rounded-md py-1 px-2 outline-none"
                       type="text"
                       value={answer}
                       placeholder="Write a comment"
